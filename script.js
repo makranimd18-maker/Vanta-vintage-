a7a6aef7d3877bb849d44168f694b4e4dd09044b
makranimd18-maker <makranimd18@gmail.com>
2026-08-11 10:15:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log("VANTA Loaded");
+const productImages = [
+  "assets/images/product1-1.png",
+  "assets/images/product1-2.png",
+  "assets/images/product1-3.png"
+];
+
+let currentProductImage = 0;
+
+function showProductImage(index) {
+  currentProductImage = index;
+
+  document.getElementById("product1-image").src =
+    productImages[currentProductImage];
+
+  const dots = document.querySelectorAll(".dot");
+
+  dots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === currentProductImage);
+  });
+}
+
+function changeProductImage(direction) {
+  currentProductImage += direction;
+
+  if (currentProductImage < 0) {
+    currentProductImage = productImages.length - 1;
+  }
+
+  if (currentProductImage >= productImages.length) {
+    currentProductImage = 0;
+  }
+
+  showProductImage(currentProductImage);
+}
